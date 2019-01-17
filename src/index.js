f83a3efa68d36fa4ef6a9a8fe46028f15c28ed0e
hz <hz932@qq.com>
2019-01-17 18:44:35 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -31,7 +31,8 @@ module.exports = class IconfontPlugin {
         // 生成的字体和css会被作为源码被提交，故npm run build时不需再次生成。
         // 而生成的字体和css在npm run dev时也会被webpack监听（watch），所以只需在svg变动时，重新生成字体和css即可
         if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === undefined) {
-            this.getBinder(compiler, 'run')(this.compile);
+            //this.getBinder(compiler, 'run')(this.compile);
+            this.compile()
             this.watch();
         }
     }
